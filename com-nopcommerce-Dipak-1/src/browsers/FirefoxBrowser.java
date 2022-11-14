2285dd934e62690abe3cb946368a08116372c0dc
primetraineesix <primetraineesix@gmail.com>
2022-11-14 11:07:11 +0000

    Dipak

---
+++ com-nopcommerce-Dipak-1/src/browsers/FirefoxBrowser.java
@@ -0,0 +1,57 @@
+package browsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FirefoxBrowser {
+
+    //Firefox
+
+    public static void main(String[] args) {
+        String baseUrl = "https://demo.nopcommerce.com/login?returnUrl=%2F";
+        System.setProperty("webdriver.firefox.driver", "drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        // Lanch the URL
+        driver.get(baseUrl);
+
+        // Maximise Window
+        driver.manage().window().maximize();
+
+        //We give implicit time to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        // Get the title of the page
+        String title = driver.getTitle();
+        System.out.println("Page title is : " + title);
+
+        // Get current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        // Get page Source
+        System.out.println("Page Source: " + driver.getPageSource());
+
+        // navigate to login page by clicking on login link
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        loginLink.click();
+
+        //Find the email field element
+        WebElement emailField = driver.findElement(By.id("Email"));
+
+        //type email to email field
+        emailField.sendKeys("test123@gmail.com");
+
+        // find the password field element
+        WebElement passwordField = driver.findElement(By.name("Password"));
+        passwordField.sendKeys("test123");
+
+        //close the browser
+        driver.quit();
+
+    }
+
+}
